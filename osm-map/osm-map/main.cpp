64fb649640505c9e221534c97fe6105eec6066ed
CodingYue <this.isssssyy@gmail.com>
2015-01-03 17:34:34 +0800

    first commit

---
+++ osm-map/osm-map/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  osm-map
+//
+//  Created by yy on 1/3/15.
+//  Copyright (c) 2015 yy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
