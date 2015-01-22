543dda2e5ea473ff12697a0398bdc73649214c27
CodingYue <this.isssssyy@gmail.com>
2015-01-22 14:26:57 +0800

    ..

--- osm-map/osm-map/main.cpp
+++ osm-map/osm-map/main.cpp
@@ -12,6 +12,7 @@
 #include <Wt/WPushButton>
 #include <Wt/WPainter>
 #include <Wt/WText>
+#include <Wt/WSvgImage>
 #include <Wt/WPaintedWidget>
 #include "xml-parser.h"
 #include <Wt/WPainterPath>
@@ -26,13 +27,22 @@ using namespace Wt;
  * to events, read input, and give feed-back.
  */
 
+typedef tuple<int, int, int> tdd;
+
+double my_height, my_width;
 void draw_image(WPainter &painter, double width, double height, int level) {
-	
+
+	painter.fillRect(0, 0, width, height, WColor(247, 245, 240));
 	for (auto &way : map_way) {
 		
+		int way_level = way.second.level;
+		if (way_level - 4 > level) continue;
 		
 		bool set_first = false;
 		WPainterPath path = WPainterPath(WPointF(0, 0));
+		
+		WPointF begin = WPointF(0, 0);
+		WPointF end = WPointF(0, 0);
 		
 		for (auto &node : way.second.node_refs) {
 			if (!map_node.count(node)) continue;
@@ -40,37 +50,142 @@ void draw_image(WPainter &painter, double width, double height, int level) {
 			double x = (map_node[node].lon - minLon) / (maxLon - minLon) * width;
 			double y = (maxLat - map_node[node].lat) / (maxLat - minLat) * height;
 			
-			printf("%.f %.f\n", x, y);
+			//printf("%.f %.f\n", x, y);
 			
 			if (!set_first) {
 				path.moveTo(WPointF(x, y));
+				begin = WPointF(x, y);
 				set_first = true;
 			} else {
 				path.lineTo(WPointF(x, y));
 			}
+			end = WPointF(x, y);
 			
 		}
+		WPen pen;
+		pen.setColor(way.second.color);
+		double c = 1-level*0.015;
+		pen.setWidth(way.second.width*c);
 		
+	//	printf("%d %d %d\n", way.second.color.red(), way.second.color.green(), way.second.color.blue());
+		
+		if (way.second.color.red() == 0 && way.second.color.green() == 0 && way.second.color.blue() == 0) {
+			cout << way.second.tag_k << " " << way.second.tag_v << endl;
+		}
+		/*
+		if (way.second.color == WColor(0, 0, 0)) {
+			cout << way.second.tag_k << " " << way.second.tag_v << endl;
+		}*/
+		
+		//printf("%d %d %d\n", way.second.color.red(), way.second.color.green(), way.second.color.blue());
+		
+		WBrush brush;
+		brush.setStyle(NoBrush);
+		
+		if (begin == end) {
+			brush.setStyle(SolidPattern);
+			brush.setColor(way.second.color);
+		}
+		painter.setBrush(brush);
+		painter.setPen(pen);
 		painter.drawPath(path);
+
 	}
 	//painter.end();
 }
 
-double my_height, my_width;
+Coordinates operator - (const Coordinates&a, const Coordinates&b) {
+	return Coordinates(a.x-b.x, a.y-b.y);
+}
 
 class MyPaintedWidget : public WPaintedWidget {
 public:
-	MyPaintedWidget(WContainerWidget *parent = 0) : WPaintedWidget(parent) {
+	MyPaintedWidget(WContainerWidget *parent = 0, int __level = 1) : WPaintedWidget(parent) {
 		resize(my_width, my_height);
+		
+		mouseWheel().connect(this, &MyPaintedWidget::mWheel);
+		mouseWentDown().connect(this, &MyPaintedWidget::mouseDown);
+		mouseDragged().connect(this, &MyPaintedWidget::mouseDrag);
+		
+		level = __level;
+		double c = 1+0.15*(level-1);
+		width_lv = c * my_width;
+		height_lv = c * my_height;
 	}
+	
 protected:
 	void paintEvent(Wt::WPaintDevice *paintDevice) {
 		WPainter painter(paintDevice);
-		painter.drawLine(0, 0, 100, 100);
-		draw_image(painter, my_width, my_height, level);
+		//painter.drawLine(0, 0, 100, 100);
+		//draw_image(painter, my_width*level, my_height*level, level);
+		char str[30];
+		sprintf(str, "./Resources/images/level_%02d.svg", level);
+		
+		
+		if (ox > width_lv - my_width) ox = width_lv - my_width;
+		if (oy > height_lv - my_height) oy = height_lv - my_height;
+		if (ox < 0) ox = 0;
+		if (oy < 0) oy = 0;
+
+		
+		paintDevice->drawImage(WRectF(0, 0, paintDevice->width().toPixels(), paintDevice->width().toPixels()), str, width_lv+10, height_lv+10, WRectF(ox, oy, my_width, my_height));
 	}
 private :
 	int level;
+	int count = 1;
+	int delta = 0;
+	double width_lv, height_lv;
+	double ox = 0, oy = 0;
+	Coordinates pre = Coordinates(0, 0);
+	
+	void mWheel(const WMouseEvent &e) {
+		delta = e.wheelDelta();
+		
+		level += (delta > 0 ? 1 : -1);
+		if (level > 20) {
+			level = 20;
+			return;
+		}
+		if (level < 1) {
+			level = 1;
+			return;
+		}
+		double c = 1+0.15*(level-1);
+		width_lv = c * my_width;
+		height_lv = c * my_height;
+//		printf("%d\n", level);
+		update();
+		
+	}
+	
+	void moveTo(const Coordinates &d) {
+		ox += d.x;
+		oy += d.y;
+		printf("%d %d\n", d.x, d.y);
+
+		
+		
+		printf("%.3f %.3f\n", width_lv, height_lv);
+		printf("%.3f %.3f\n", ox, oy);
+		
+		update();
+	}
+	
+	void mouseDrag(const Wt::WMouseEvent& e) {
+		Wt::Coordinates c = e.widget();
+		
+		Coordinates d = (pre - c);
+		
+		//printf("%d %d\n", d.x, d.y);
+		
+		moveTo(d);
+		
+		pre = c;
+	} void mouseDown(const Wt::WMouseEvent& e) {
+		pre = e.widget();
+	}
+
+	
 };
 
 class MapApplicatoin : public WApplication {
@@ -86,7 +201,9 @@ MapApplicatoin::MapApplicatoin(const WEnvironment& env) : WApplication(env) {
 	root()->addWidget(new WText("Your name, please ? "));
 	WContainerWidget *containter = new WContainerWidget(root());
 	
-	WPaintedWidget *painting = new MyPaintedWidget(containter);
+	
+	WPaintedWidget *painting = new MyPaintedWidget(containter, 1);
+	
 	
 	
 }
@@ -122,13 +239,36 @@ int main(int argc, char **argv)
 	 * support. The function should return a newly instantiated application
 	 * object.
 	 */
+	
+	
+	load_fliter("./Resources/level_fliter.xml");
 	parse_file("./Resources/map2.osm");
 	my_height = 900.0;
 	my_width = (maxLon - minLon) / (maxLat - minLat) * my_height;
-	
+	/*
 	for (auto &way : map_way) {
-		for (auto &node : way.second.node_refs) printf("%ld ", node);
+		for (auto &node : way.second.node_refs) {
+			if (!map_node.count(node)) continue;
+			printf("%f %f\n", map_node[node].lon, map_node[node].lat);
+		}
 		puts("");
+	}
+	*/
+	
+	for (int level = 1; level <= 20; ++level) {
+		
+		
+		double c = 1+0.15*(level-1);
+		WSvgImage svgImage(c*my_width+10, c*my_height+10);
+		WPainter painter(&svgImage);
+		draw_image(painter, c*my_width, c*my_height, level);
+	//	painter.end();
+		
+		char str[30];
+		sprintf(str, "./Resources/images/level_%02d.svg", level);
+		
+		ofstream f(str);
+		svgImage.write(f);
 	}
 	
 	return WRun(argc, argv, &createApplication);
--- osm-map/osm-map/xml-parser.h
+++ osm-map/osm-map/xml-parser.h
@@ -12,8 +12,19 @@
 
 #include <libxml/tree.h>
 #include <libxml/parser.h>
+#include <string.h>
+#include <stdio.h>
 #include <vector>
 #include <map>
+#include <Wt/WApplication>
+#include <Wt/WBreak>
+#include <Wt/WContainerWidget>
+#include <Wt/WLineEdit>
+#include <Wt/WPushButton>
+#include <Wt/WPainter>
+#include <Wt/WText>
+#include <Wt/WPaintedWidget>
+#include <Wt/WPainterPath>
 using namespace std;
 
 class Node {
@@ -27,21 +38,77 @@ public:
 
 class Way {
 public:
+	int level;
 	vector<long> node_refs;
-	string tag;
+	Wt::WColor color;
+	double width;
+	string tag_k, tag_v;
 	
-	Way(){}
-	Way(const vector<long> &__node_refs, const string &__tag) {
+	Way(){
+		color = Wt::WColor(0, 0, 0);
+	}
+	Way(const vector<long> &__node_refs, const int &__level, const Wt::WColor &__color, const double &__width,
+		char* __tag_k, char *__tag_v
+		) {
 		node_refs = __node_refs;
-		tag = __tag;
+		level = __level;
+		color = __color;
+		width = __width;
+		tag_k = __tag_k;
+		tag_v = __tag_v;
 	}
 	
 };
 
+typedef pair<string, string> pss;
+
 map<long, Node> map_node;
 map<long, Way> map_way;
+map<pss, int> level_hash;
+map<pss, Wt::WColor> color_hash;
+map<pss, double> width_hash;
 
 double minLat, minLon, maxLat, maxLon;
+
+const char* way_type[] = {"natural", "waterway", "highway", "leisure", "landuse", "building", "aeroway", "tourism"};
+
+int load_fliter(char *path) {
+	xmlDocPtr doc = xmlParseFile(path);
+	assert(doc != NULL);
+	
+	xmlNodePtr root = xmlDocGetRootElement(doc);
+	
+	for (xmlNodePtr child = root->children; child; child = child->next) {
+		if (xmlStrcasecmp(child->name, BAD_CAST"road") == 0) {
+			int level = atof((char*) xmlGetProp(child, BAD_CAST"level"));
+			
+			for (xmlNodePtr tag = child->children; tag; tag = tag->next) {
+				if (xmlStrcasecmp(tag->name, BAD_CAST"tag") != 0) continue;
+				char *tag_k = (char *) xmlGetProp(tag, BAD_CAST"k");
+				char *tag_v = (char *) xmlGetProp(tag, BAD_CAST"v");
+			//	printf("%s %s\n", tag_k, tag_v);
+				level_hash[make_pair(tag_k, tag_v)] = level;
+				
+				
+				char *color_str = (char *) xmlGetProp(tag, BAD_CAST"rgb");
+				char *width_str = (char *) xmlGetProp(tag, BAD_CAST"width");
+				
+				int r = 0, g = 0, b = 0;
+				if (color_str != NULL) sscanf(color_str, "%d %d %d", &r, &g, &b);
+				double width = 1.0;
+				if (width_str != NULL) width = atof(width_str) / 7.0;
+			//	printf("%.10f\n", width);
+				color_hash[make_pair(tag_k, tag_v)] = Wt::WColor(r, g, b);
+				width_hash[make_pair(tag_k, tag_v)] = width;
+			}
+			
+		}
+	}
+	
+	xmlFreeDoc(doc);
+	return 0;
+	
+}
 
 int parse_file(char *path) {
 	
@@ -67,20 +134,37 @@ int parse_file(char *path) {
 			
 			long name = atol((char*) xmlGetProp(child, BAD_CAST"id"));
 			vector<long> refs;
-			string tag;
-			
+			int level = 0;
+			double width = 0;
+			Wt::WColor color;
+			char *tag_k = "", *tag_v = "";
+			bool fuck = false;
 			for (xmlNodePtr way_ch = child->children; way_ch; way_ch = way_ch->next) {
 				if (xmlStrcasecmp(way_ch->name, BAD_CAST"nd") == 0) {
 					refs.push_back(atol((char*) xmlGetProp(way_ch, BAD_CAST"ref")));
 				}
 				if (xmlStrcasecmp(way_ch->name, BAD_CAST"tag") == 0) {
-					if (xmlStrcasecmp(xmlGetProp(way_ch, BAD_CAST"k"), BAD_CAST"highway") == 0) {
-						tag = (char*) xmlGetProp(way_ch, BAD_CAST"v");
+
+					for (int i = 0; i < 8; ++i) {
+						tag_k = (char *) xmlGetProp(way_ch, BAD_CAST"k");
+						if (strcmp(tag_k, way_type[i]) != 0) continue;
+
+						if (i < 5) {
+							tag_v = (char* ) xmlGetProp(way_ch, BAD_CAST"v");
+						}
+						if (!level_hash.count(make_pair(tag_k, tag_v))) {
+							continue;
+						}
+						fuck = true;
+						level = level_hash[make_pair(tag_k, tag_v)];
+						color = color_hash[make_pair(tag_k, tag_v)];
+						width = width_hash[make_pair(tag_k, tag_v)];
 					}
 				}
 			}
-			
-			map_way[name] = Way(refs, tag);
+		//	printf("%.10f\n", width);
+			if (!fuck) continue;
+			map_way[name] = Way(refs, level, color, width, tag_k, tag_v);
 			
 		}
 		
